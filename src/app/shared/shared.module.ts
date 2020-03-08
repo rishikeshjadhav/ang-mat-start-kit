a31245c1909eed0222acce71b65409b73a6bdd96
Rishikesh Jadhav <rishijadhav2792@gmail.com>
2020-03-08 15:15:16 +0530

    Added CommonModule and FormsModule to SharedModule

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { materialImports } from './material-imports';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
+    FormsModule,
     materialImports
   ],
   exports: [
+    CommonModule,
+    FormsModule,
     materialImports
   ]
 })
