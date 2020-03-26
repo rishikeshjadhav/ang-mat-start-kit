102932b19bf035c45dbca4564ac5eb19c7c0e723
Rishikesh Jadhav <rishijadhav2792@gmail.com>
2020-03-26 20:07:18 +0530

    Added Nav bar component with responsive side bar

---
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -0,0 +1,130 @@
+import { Component, OnInit, ElementRef } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
+})
+export class NavBarComponent implements OnInit {
+
+  private mobileMenuVisible: any = 0;
+  private toggleButton: any;
+  private sidebarVisible: boolean;
+  private sidebarMini = false;
+
+  constructor(
+    private element: ElementRef,
+    private router: Router,
+  ) { }
+
+  ngOnInit() {
+    const navbar: HTMLElement = this.element.nativeElement;
+    this.toggleButton = navbar.getElementsByClassName('navbar-toggler')[0];
+    this.router.events.subscribe(event => {
+      this.sidebarClose();
+      const layer: any = document.getElementsByClassName('close-layer')[0];
+      if (layer) {
+        layer.remove();
+        this.mobileMenuVisible = 0;
+      }
+    });
+  }
+
+
+  sidebarOpen() {
+    const toggleButton = this.toggleButton;
+    const body = document.getElementsByTagName('body')[0];
+    setTimeout(() => {
+      toggleButton.classList.add('toggled');
+    }, 500);
+
+    body.classList.add('nav-open');
+
+    this.sidebarVisible = true;
+  }
+  sidebarClose() {
+    const body = document.getElementsByTagName('body')[0];
+    this.toggleButton.classList.remove('toggled');
+    this.sidebarVisible = false;
+    body.classList.remove('nav-open');
+  }
+  sidebarToggle() {
+    // const toggleButton = this.toggleButton;
+    // const body = document.getElementsByTagName('body')[0];
+    const $toggle = document.getElementsByClassName('navbar-toggler')[0];
+
+    if (this.sidebarVisible === false) {
+      this.sidebarOpen();
+    } else {
+      this.sidebarClose();
+    }
+    const body = document.getElementsByTagName('body')[0];
+
+    if (this.mobileMenuVisible === 1) {
+      // $('html').removeClass('nav-open');
+      body.classList.remove('nav-open');
+      // if (layer) {
+      //     layer.remove();
+      // }
+      setTimeout(() => {
+        $toggle.classList.remove('toggled');
+      }, 400);
+
+      this.mobileMenuVisible = 0;
+    } else {
+      setTimeout(() => {
+        $toggle.classList.add('toggled');
+      }, 430);
+
+      const $layer = document.createElement('div');
+      $layer.setAttribute('class', 'close-layer');
+
+      if (body.querySelectorAll('.main-panel')) {
+        document.getElementsByClassName('main-panel')[0].appendChild($layer);
+      } else if (body.classList.contains('off-canvas-sidebar')) {
+        document
+          .getElementsByClassName('wrapper-full-page')[0]
+          .appendChild($layer);
+      }
+
+      setTimeout(() => {
+        $layer.classList.add('visible');
+      }, 100);
+
+      $layer.onclick = function() {
+        // asign a function
+        body.classList.remove('nav-open');
+        this.mobileMenuVisible = 0;
+        $layer.classList.remove('visible');
+        setTimeout(() => {
+          $layer.remove();
+          $toggle.classList.remove('toggled');
+        }, 400);
+      }.bind(this);
+
+      body.classList.add('nav-open');
+      this.mobileMenuVisible = 1;
+    }
+  }
+
+  sidebarMinToggle() {
+    const body = document.getElementsByTagName('body')[0];
+    if (this.sidebarMini) {
+      body.classList.remove('sidebar-mini');
+      this.sidebarMini = false;
+    } else {
+      body.classList.add('sidebar-mini');
+      this.sidebarMini = true;
+    }
+  }
+
+  getTitle() {
+    return 'Dashboard';
+  }
+
+  logout() {
+    window.location.reload();
+  }
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { materialImports } from './material-imports';
 import { FormsModule } from '@angular/forms';
+
+import { materialImports } from './material-imports';
 import { SharedRoutingModule } from './shared-routing.module';
 import { NavigationComponent } from './components/navigation/navigation.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { NavBarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
-  declarations: [NavigationComponent, SidebarComponent],
+  declarations: [
+    NavigationComponent,
+    SidebarComponent,
+    NavBarComponent
+  ],
   imports: [
     CommonModule,
     FormsModule,
